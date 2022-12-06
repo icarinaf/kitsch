f055c68b65e7a098be1b17099f85d4e25f838959
Carina Fung <icarinaf@stanford.edu>
2022-12-06 15:17:38 -0800

    selecting location adds review to correct place

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,48 +1,29 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
-import AntDesign from 'react-native-vector-icons/AntDesign';
 import { Ionicons } from '@expo/vector-icons';
-import { supabase } from '../supabase';
 
-// const data = [
-//   { label: 'Item 1', value: '1' },
-//   { label: 'Item 2', value: '2' },
-//   { label: 'Item 3', value: '3' },
-//   { label: 'Item 4', value: '4' },
-//   { label: 'Item 5', value: '5' },
-//   { label: 'Item 6', value: '6' },
-//   { label: 'Item 7', value: '7' },
-//   { label: 'Item 8', value: '8' },
-// ];
 
-const DropdownComponent = (places) => {
+const DropdownComponent = (props) => {
+  const places = props.places;
+  //console.log("props--", props);
+  const getSelectedPlace = props.getSelectedPlace;
+  //console.log(props.getSelectedPlace);
   const [value, setValue] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
+  const [selectedId, setSelectedId] = useState(0);
   const data = [];
 
   const createLabels = () => {
-    if (places.places.length !== 0) {
-      console.log("places--", places);
-      console.log("places- ", places.places);
-      const placeList = places.places;
-      console.log("placeList-- ", placeList);
-      for (var i = 0; i < placeList.length; i++) {
-        console.log("placelist num ", placeList[i]);
-        console.log('placelist id', placeList[i].id);
-        const label_txt = placeList[i].name;
-        console.log(label_txt);
-        const label_id = placeList[i].id;
+    if (places && places.length !== 0) {
+      //console.log("places--", places);
+      for (var i = 0; i < places.length; i++) {
+        const label_txt = places[i].name;
+        const label_id = places[i].id;
         data.push({label: label_txt, value: label_id});
-        console.log(data);
+        //console.log(data);
       }
-      //console.log(places.places[0]);
-      // for (let i = 1; i < places.places.length; i++) {
-      //     const label_txt = places.places[i].name;
-      //     data.push({label: {label_txt}, value: i})
-      //     console.log(data);
-      // }
     }
   }
 
@@ -74,6 +55,8 @@ const DropdownComponent = (places) => {
           setValue(item.value);
           setIsFocus(false);
           setIsSelected(true);
+          getSelectedPlace(item.value);
+          //console.log("item value: " + item.value);
         }}
         renderLeftIcon={() => (
             <Ionicons style={styles.icon} name={isSelected ? "location" : "location-outline"} size={20}/>
@@ -97,6 +80,7 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderRadius: 8,
     paddingHorizontal: 8,
+    color: 'white',
   },
   label: {
     position: 'absolute',
@@ -106,12 +90,14 @@ const styles = StyleSheet.create({
     zIndex: 999,
     paddingHorizontal: 8,
     fontSize: 10,
+    color: 'white',
   },
   placeholderStyle: {
     fontSize: 12,
   },
   selectedTextStyle: {
     fontSize: 12,
+
   },
   icon: {
     marginRight: 5,
